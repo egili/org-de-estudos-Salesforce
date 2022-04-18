b756889aaf91b524c606138b0c634bd72dcbcc24
eliseu gili <gili.eliseup@gmail.com>
2022-04-18 20:09:47 -0300

    a

--- force-app/main/default/lwc/ratingList/ratingList.js
+++ force-app/main/default/lwc/ratingList/ratingList.js
@@ -1,7 +1,16 @@
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import { LightningElement, wire } from 'lwc';
 
+const colums = [
+    { label: 'Título', fieldName: 'name' },
+    { label: 'Nota', fieldName: 'nota' },
+    { label: 'Descrição', fieldName: 'descricao' },
+    {label: 'Autor', fieldName: 'autor' }
+];
+
 export default class RatingList extends LightningElement {
+
+    colums = colums;
 
     @wire(getObjectInfo, {objectApiName: 'Avaliacao__c'})
     avaliacaoMetadata;
