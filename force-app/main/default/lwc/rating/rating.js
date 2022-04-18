595786a05affe35809b740fece6d14fc8fb5339f
eliseu gili <gili.eliseup@gmail.com>
2022-04-18 15:03:36 -0300

    nn

--- force-app/main/default/lwc/rating/rating.js
+++ force-app/main/default/lwc/rating/rating.js
@@ -1,15 +1,19 @@
 import { LightningElement, wire } from 'lwc';
 import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import includeRating from '@salesforce/apex/ratingController.includeRating';
 
 export default class Rating extends LightningElement {
 
     @wire(getObjectInfo, { objectApiName: "Avaliacao__c"})
     avaliacaoMetadata;
-    
     @wire(getPicklistValues, { recordTypeId: "$avaliacaoMetadata.data.defaultRecordTypeId", fieldApiName: "Avaliacao__c.Nota__c"})
     notaPicklist;
 
-    handleChange(event) {
+    notaSelected(event) {
         this.value = event.detail.value;
+    }
+
+    saveClick(event) {
+
     }
 }
