7d2fde8f4a7f2a17f05e99470707dbefc5e243ab
eliseu gili <gili.eliseup@gmail.com>
2022-04-19 13:58:59 -0300

    a

--- force-app/main/default/lwc/ratingList/ratingList.js
+++ force-app/main/default/lwc/ratingList/ratingList.js
@@ -1,5 +1,5 @@
+import { LightningElement, track, wire, api } from 'lwc';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-import { LightningElement, track, wire } from 'lwc';
 import getAvaliacaoList from '@salesforce/apex/ratingListController.getAvaliacaoList';
 
 export default class RatingList extends LightningElement {
@@ -29,26 +29,57 @@ export default class RatingList extends LightningElement {
         type: 'lookup'
     }];*/
 
+    @track data;
+    @api recordId;
+
     @wire(getObjectInfo, {objectApiName: 'Avaliacao__c'})
     avaliacaoMetadata;
     
-    @wire(getAvaliacaoList)
-    avaliacaoList;
-
+    @wire(getAvaliacaoList , { avaliacaoId : '$recordId' })
+    avaliacaoList({error, data}) {
+        if(data){
+            this.dados = data;
+            this.error = undefined;
+        } else if(error){
+            this.error = error;
+            this.data = undefined;
+            console.log('errors ' , error);
+        }
+    }
+    
     get tituloLabel() {
         if(this.avaliacaoMetadata)
-            return this.avaliacaoMetadata.data.fields.Name.label;
+        return this.avaliacaoMetadata.data.fields.Name.label;
     }
+    get tituloData() {
+        if(this.avaliacaoList || this.recordId)
+            return this.avaliacaoList.data.Name;
+    }
+
     get notaLabel() {
         if(this.avaliacaoMetadata)
             return this.avaliacaoMetadata.data.fields.Nota__c.label;
     }
+    get notaData() {
+        if(this.avaliacaoList)
+            return this.avaliacaoList.data.fields.Nota__c;
+    }
+
     get descricaoLabel() {
         if(this.avaliacaoMetadata)
             return this.avaliacaoMetadata.data.fields.Descricao__c.label;
     }
+    get descricaoData() {
+        if(this.avaliacaoList)
+            return this.avaliacaoList.data.fields.Descricao__c;
+    }
+
     get autorLabel() {
         if(this.avaliacaoMetadata)
             return this.avaliacaoMetadata.data.fields.OwnerId.label;
+    }
+    get autorData() {
+        if(this.avaliacaoList)
+            return this.avaliacaoList.data.fields.OwnerId;
     }
 }
